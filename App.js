60c692d4ca82172c7339cd0c60f74db2459a2dbb
Jesus Vasquez <jesus.i.vasq@gmail.com>
2021-02-12 00:10:16 -0600

    parsed commit data and saved to component state

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import {
   Text,
 } from 'react-native';
 
-const PUBLIC_REPO = "";
+const PUBLIC_REPO = "https://api.github.com/repos/JesusVasq/GM_Technical_Challenge/commits?sha=develop&per_page=50";
 
 const App = () => {
 
@@ -14,14 +14,29 @@ const App = () => {
 
   useEffect(() => {
     console.log("Hello");
-    fetch("https://api.github.com/repos/JesusVasq/GM_Technical_Challenge/commits?sha=develop&per_page=50", {method: "GET"})
+    fetch(PUBLIC_REPO, {method: "GET"})
     .then(response => response.json())
-    .then(result => console.log(result))
+    .then(result => parseData(result))
     .catch(error => console.log('error', error));
   },[])
 
   let parseData = (commitJSONArray) =>{
     console.log("Parsing data!");
+
+    let tempCommitList = [];
+
+    for(var i = 0; i < commitJSONArray.length; i ++){
+      let single_commit = commitJSONArray[i];
+
+      let sha = single_commit.sha;
+      let author = single_commit.commit.author.name;
+      let message = single_commit.commit.message;
+      
+      tempCommitList.push({sha: sha, author: author, message: message});
+    }
+
+    console.log(tempCommitList);
+    setCommitList(tempCommitList);
   }
 
   return (
@@ -31,7 +46,7 @@ const App = () => {
         justifyContent: "center",
         alignItems: "center"
       }}>
-      <Text>Hello, World!</Text>
+      <Text>Hello, poop!</Text>
     </View>
   )
 }
